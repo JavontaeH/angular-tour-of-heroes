59149ce4a1627224a6a3034379d797447d595cec
Javontae.Hardeman <Javontae.Hardeman@navihealth.com>
2022-11-16 11:28:58 -0600

    finished adding services

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../services/hero.service';
+import { MessageService } from '../services/message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,13 +9,27 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
-  constructor() {}
-  heroes = HEROES;
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService
+  ) {}
+
+  heroes: Hero[] = [];
+
   selectedHero?: Hero;
-  ngOnInit(): void {}
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
+  }
+
   onSelect(hero: Hero): void {
     this.selectedHero !== hero
       ? (this.selectedHero = hero)
       : (this.selectedHero = undefined);
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 }
---
+++ src/app/services/message.service.spec.ts
@@ -0,0 +1,15 @@
+import { MessageService } from './message.service';
+import { TestBed } from '@angular/core/testing';
+
+describe('MessagesService', () => {
+  let service: MessageService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(MessageService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
